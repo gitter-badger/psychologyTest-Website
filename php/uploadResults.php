84415977273e493db3c694bd7ae81a17e62931ec
StefanGramadnikov <emikak1@gmail.com>
2015-05-09 18:36:02 +0300

    update

--- php/uploadResults.php
+++ php/uploadResults.php
@@ -1,42 +1,20 @@
 <?php
 
+require_once('db.php');
+require_once('ParamsValidator.php');
+
 error_reporting(E_ALL);
 
-//include 'db.php';
-//$query = "INSERT INTO results(";
-//
-//foreach ($_POST as $key => $val) {
-//    $query .= $key . ",";
-//}
-//$query = rtrim(trim($query),",");
-//
-//$query .= ") VALUES (";
-//
-//foreach ($_POST as $key => $val) {
-//    $query .= $val . ",";
-//}
-//$query = rtrim(trim($query),",");
-//
-//$query .= ")";
-//
-//
-//var_dump($query);
-//
-//if ($mysqli->query($query)) {
-//    echo "success";
-//}
-//else {
-//    echo "error" . $mysqli->error;
-//}
-//
-//$mysqli->close();
-
-require_once('db.php');
+$data = $_POST;
 
 $db = new MysqliDb ('localhost', 'root', '', 'psychology');
+$paramsValidator = new ParamsValidator();
 
-$id = $db->insert ('results', $_POST);
-if ($id)
-    echo 'user was created. Id=' . $id;
-else
-    echo 'insert failed: ' . $db->getLastError();
+if ($paramsValidator->validate($data)) {
+    $id = $db->insert ('results', $_POST);
+    if ($id == false) {
+        http_response_code(500);
+    }
+} else {
+    http_response_code(500);
+}
